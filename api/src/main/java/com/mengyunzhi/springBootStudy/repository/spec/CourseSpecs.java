a8ac9de5e84f970ddf5a9cd6b8237925ac763acd
chenjunnian <3112037487@qq.com>
2025-01-19 16:08:35 +0800

    综合查询完成

--- api/src/main/java/com/mengyunzhi/springBootStudy/repository/spec/CourseSpecs.java
+++ api/src/main/java/com/mengyunzhi/springBootStudy/repository/spec/CourseSpecs.java
@@ -22,16 +22,20 @@ public class CourseSpecs {
                     Join<Course, Term> termJoin = root.join("term");
                     Join<Term, School> schoolJoin = termJoin.join("school");
 
+                    //定义子查询语句
                     Subquery<Long> subquery = criteriaQuery.subquery(Long.class);
+                    //构建子查询语句的from成分
                     Root<School> schoolSubRoot = subquery.from(School.class);
+                    //join klass表
                     Join<School, Klass> klassJoin = schoolSubRoot.join("klass"); // klass是School中Klass列表的属性名
-                    subquery.select(schoolSubRoot.get("id")); // 选择School的ID
+                    //子查询的select部分
+                    subquery.select(schoolSubRoot.get("id"));
+                    //构建where查询条件语句
                     Predicate klassPredicate = criteriaBuilder.equal(klassJoin.get("id"), klass.getId()); // 比较Klass的ID
                     subquery.where(klassPredicate);
 
-                    // 在主查询中使用IN子句来检查School的ID是否在子查询结果中
+                    // 在主查询中使用IN子句来检查School的ID是否在子查询结果中，选择那些其School的id在子查询结果集中的Course实体
                     Predicate inPredicate = criteriaBuilder.in(schoolJoin.get("id")).value(subquery);
-
                     return inPredicate;
                 }
             };
@@ -50,7 +54,10 @@ public class CourseSpecs {
             return new Specification<Course>() {
                 @Override
                 public Predicate toPredicate(Root<Course> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                    return criteriaBuilder.equal(root.get("school").as(School.class), school);
+                    Join<Course, Term> termJoin = root.join("term");
+                    Join<Term, School> schoolJoin = termJoin.join("school");
+
+                    return criteriaBuilder.equal(schoolJoin.get("id"), school.getId());
                 }
             };
         }
@@ -68,7 +75,8 @@ public class CourseSpecs {
             return new Specification<Course>() {
                 @Override
                 public Predicate toPredicate(Root<Course> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                    return criteriaBuilder.equal(root.get("user").as(User.class), user);
+                    Join<Course, User> userJoin = root.join("users", JoinType.INNER);
+                    return criteriaBuilder.equal(userJoin.get("id"), user.getId());
                 }
             };
         }
@@ -81,6 +89,7 @@ public class CourseSpecs {
      * */
     public static Specification<Course> belongToTerm(Term term) {
         if(term == null || null == term.getId()) {
+            System.out.println("term为null");
             return Specification.where(null);
         }else {
             return new Specification<Course>() {
