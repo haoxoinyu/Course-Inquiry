2c5993e6bbda1f4992eb344ccd6366df8be79bde
lizixuan <3072613174@qq.com>
2025-01-23 04:02:54 +0800

    68

--- web-app/src/app/schedule/schedule.component.spec.ts
+++ web-app/src/app/schedule/schedule.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScheduleComponent } from './schedule.component';
+import {CourseScheduleComponent} from "../course-schedule/course-schedule.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('ScheduleComponent', () => {
   let component: ScheduleComponent;
@@ -8,7 +11,11 @@ describe('ScheduleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ScheduleComponent ]
+      declarations: [ ScheduleComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
     })
     .compileComponents();
 
--- web-app/src/app/service/schedule.service.spec.ts
+++ web-app/src/app/service/schedule.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ScheduleService } from './schedule.service';
+import {CourseScheduleComponent} from "../course-schedule/course-schedule.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('ScheduleService', () => {
   let service: ScheduleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [ ScheduleService ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+    });
     service = TestBed.inject(ScheduleService);
   });
 
