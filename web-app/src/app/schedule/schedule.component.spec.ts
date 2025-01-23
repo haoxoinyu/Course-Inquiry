6d258401d46593c1177e526522f4fe4f723bf258
lizixuan <3072613174@qq.com>
2025-01-23 14:14:25 +0800

    68

--- web-app/src/app/schedule/schedule.component.spec.ts
+++ web-app/src/app/schedule/schedule.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScheduleComponent } from './schedule.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 
 describe('ScheduleComponent', () => {
   let component: ScheduleComponent;
@@ -8,9 +11,16 @@ describe('ScheduleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ScheduleComponent ]
+      declarations: [ ScheduleComponent ],
+      imports: [ HttpClientTestingModule,
+        ReactiveFormsModule,
+        MatDialogModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ScheduleComponent);
     component = fixture.componentInstance;
