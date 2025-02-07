31975cd3ef62418ee5424f8ae4f68c0baefb95d1
haoxoinyu <1014590194@qq.com>
2025-02-07 20:17:12 +0800

    修改

--- web-app/src/app/welcome/welcome.component.spec.ts
+++ web-app/src/app/welcome/welcome.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeComponent } from './welcome.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MatDialogModule} from "@angular/material/dialog";
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
@@ -8,7 +10,8 @@ describe('WelcomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ]
+      declarations: [ WelcomeComponent ],
+      imports: [HttpClientTestingModule, MatDialogModule]
     })
     .compileComponents();
   }));
